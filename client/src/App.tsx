1e47e47173425daa1eb2374bbc81fd37d649ee8c
jeffigy <ejeffydev@gmail.com>
2023-10-11 07:36:02 +0800

    client side setup

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+function App() {
+  return (
+    <div className="m-5">
+      {/* <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Click me</Button> */}
+    </div>
+  );
+}
+
+export default App;
